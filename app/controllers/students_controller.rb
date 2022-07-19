f89a55f49b8f85302d6820595f7f434346fb2475
Alex Guanipatin <sebas5950@gmail.com>
2022-07-18 21:31:52 -0400

    commit tree 216becf0be8b62fc02c58283578acb5e53bf14f6

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,19 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
+    students = if params[:first_name]
+            Student.by_name(params[:first_name])
+    else 
+      Student.all
+    end
     render json: students
   end
+
+  def show
+    show = Student.find(params[:id])
+    render json: show
+  end
+
+
 
 end
